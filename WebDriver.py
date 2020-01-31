aaf24e7eb1fd239680cb5d62d47416c815a96b50
Almog Nahari <almognahari2610@gmail.com>
2020-01-31 19:33:13 +0200

    Implemented WebDriver

---
+++ WebDriver.py
@@ -0,0 +1,38 @@
+from InputBox import InputBox
+
+
+def fill_input_boxes(browser):
+    input_boxes = get_all_input_boxes(browser)
+
+    for input_box in input_boxes:
+        if input_box.name == "FirstName":
+            input_box.web_element.send_keys("Mostafa")
+
+        if input_box.name == "Username":
+            input_box.web_element.send_keys("Mosit")
+
+        if input_box.name == "Email":
+            input_box.web_element.send_keys("stordii@itchsoberlyleap.website")
+
+        if input_box.name == "Password":
+            input_box.web_element.send_keys("A1029384756")
+
+        if input_box.name == "ConfirmPassword":
+            input_box.web_element.send_keys("A1029384756")
+
+
+def get_all_input_boxes(browser):
+    first_name_web_element = browser.find_element_by_id("firstName")
+    username_web_element = browser.find_element_by_id("userName")
+    email_web_element = browser.find_element_by_id("email")
+    password_web_element = browser.find_element_by_id("password")
+    confirm_password_web_element = browser.find_element_by_id("confirmPassword")
+
+    return [InputBox("FirstName", first_name_web_element), InputBox("Username", username_web_element),
+            InputBox("Email", email_web_element), InputBox("Password", password_web_element),
+            InputBox("ConfirmPassword", confirm_password_web_element)]
+
+
+def click_sign_up_button(browser):
+    sign_up_button = browser.find_element_by_id("register")
+    sign_up_button.click()
