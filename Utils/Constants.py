cc959cfedb0d452ae5791d94d04f4667f6e18f83
Almog Nahari <almognahari2610@gmail.com>
2020-01-31 02:01:37 +0200

    Added files to a dedicated directory

--- Utils/Constants.py
+++ Utils/Constants.py
@@ -1,3 +1,4 @@
 HASTE_WEBSITE_URL = "https://haste.net"
 HASTE_FREE_TRIAL_URL = "https://account.haste.net/my-account/?hm=register"
 FAKE_EMAIL_URL = "https://emailfake.com"
+RANDOM_WORD_GENERATOR_URL = "https://randomwordgenerator.com"
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup
-import Constants
+from Utils import Constants
 
 
 def main():
