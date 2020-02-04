222e696d734856220479dc44e84c2153ea46f22f
Almog Nahari <almognahari2610@gmail.com>
2020-02-04 07:08:18 +0200

    Added HasteUser.py

--- HasteUser.py
+++ HasteUser.py
@@ -1,6 +1,6 @@
 class HasteUser:
 
-    def __int__(self, first_name, email, username, password):
+    def __init__(self, first_name, email, username, password):
         self.first_name = first_name
         self.email = email
         self.username = username
